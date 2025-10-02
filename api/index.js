fd975d86f2b849b8da2adbf6da0f2331b4016a62
careerkings <careerkingsconsult@gmail.com>
2025-10-02 05:16:35 +0100

    connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("mongoose is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.send("server is running");
+});
 
 app.listen(5000, () => {
   console.log("port is listening at 5000");
